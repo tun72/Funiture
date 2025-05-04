0c3014e9768cf3b920605656ba38d1ab3d00b3da
Tun Tun Myint <tunt72553@gmail.com>
2025-05-04 23:30:08 +0630

    finish-mobile-nav

--- client/src/components/layouts/MobileNavigation.tsx
+++ client/src/components/layouts/MobileNavigation.tsx
@@ -12,12 +12,20 @@ import {
   Sheet,
   SheetClose,
   SheetContent,
-  SheetDescription,
-  SheetFooter,
-  SheetHeader,
-  SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
+import { Icons } from "../icons";
+import { Link } from "react-router";
+import { siteConfig } from "@/config/site";
+
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+import { ScrollArea } from "../ui/scroll-area";
+
 
 // import { Icons } from "@/components/icons";
 // import { siteConfig } from "@/config/site";
@@ -29,28 +37,60 @@ interface MainNavigationProps {
 function MobileNavigation({ items }: MainNavigationProps) {
   return (
     <div className="lg:hidden">
-      {" "}
       <Sheet>
         <SheetTrigger asChild>
-          <Button variant="outline">Open</Button>
+          <Button variant="ghost" size={'icon'} className="ml-4 size-6 cursor-pointer">
+            <Icons.hamburger aria-hidden="true" />
+            <span className="sr-only">Toggle Menu</span>
+          </Button>
         </SheetTrigger>
-        <SheetContent side={"left"}>
-          <SheetHeader>
-            <SheetTitle>Edit profile</SheetTitle>
-            <SheetDescription>
-              Make changes to your profile here. Click save when you're done.
-            </SheetDescription>
-          </SheetHeader>
-          <div className="grid gap-4 py-4"></div>
-          <SheetFooter>
+
+        <SheetContent side={"left"} className="pt-9">
+          <div className="p-4">
             <SheetClose asChild>
-              <Button type="submit">Save changes</Button>
+              <Link to="/" className="flex items-center">
+                <Icons.logo className="size-5 mr-2" />
+                <span className="font-bold">{siteConfig.name}</span>
+                <span className="sr-only">Home</span>
+              </Link>
             </SheetClose>
-          </SheetFooter>
+
+            <ScrollArea className="my-4 h-[calc(100vh-8rem)] pb-8">
+              <Accordion type="multiple" className="w-full">
+                <AccordionItem value={String(items?.[0].title)}>
+                  <AccordionTrigger>{items?.[0].title}</AccordionTrigger>
+                  <AccordionContent>
+                    <div className="flex flex-col space-y-2 pl-2">
+                      {items?.[0].card.map((item) => (
+                        <SheetClose asChild key={item.title}>
+                          <Link to={String(item.href)} className="text-foreground/70">{item.title}</Link>
+                        </SheetClose>
+                      ))}
+                    </div>
+                  </AccordionContent>
+                </AccordionItem>
+
+              </Accordion>
+              <div className="flex flex-col space-y-2 mt-2">
+                {items?.[0].menu.map((menu) => (
+                  <SheetClose asChild key={menu.title}>
+                    <Link to={String(menu.href)}>{menu.title}</Link>
+                  </SheetClose>
+                ))}
+              </div>
+            </ScrollArea>
+          </div>
         </SheetContent>
+
       </Sheet>
     </div>
   );
 }
 
+
+
+
 export default MobileNavigation;
+
+
+/* */
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router'
 import { router } from './routes'
+import { ThemeProvider } from './components/theme-provider'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </StrictMode>,
 )
