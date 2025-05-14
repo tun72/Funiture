4345d5a5296cf31ee46110ad0c45162dda3152b7
Tun Tun Myint <tunt72553@gmail.com>
2025-05-14 18:31:47 +0630

    reate-limit

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,10 +3,15 @@ import helmet from "helmet";
 import compression from "compression";
 import cors from "cors";
 import morgan from "morgan";
+import { limiter } from "./middlewares/rateLimiter";
 
 export const app = express();
 
 app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
+app.use(cors()).use(helmet()).use(compression()).use(limiter);
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ message: "In Good Condition ✅ " });
+});
