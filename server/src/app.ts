77f07b935bb19685fd15dc6ed79a59351a628241
Tun Tun Myint <tunt72553@gmail.com>
2025-05-16 00:17:00 +0630

    add-postgresql-database

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,11 @@
-import express from "express";
+import express, { Response, Request, NextFunction } from "express";
 import helmet from "helmet";
 import compression from "compression";
 import cors from "cors";
 import morgan from "morgan";
 import { limiter } from "./middlewares/rateLimiter";
+import { check } from "./middlewares/check";
+import healthRoute from "./routes/v1/health";
 
 export const app = express();
 
@@ -12,6 +14,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors()).use(helmet()).use(compression()).use(limiter);
 
-app.get("/health", (req, res) => {
-  res.status(200).json({ message: "In Good Condition ✅ " });
+app.use("/api/v1", healthRoute);
+
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  const status = err.status || 500;
+  const message = err.message || "Server Error";
+  const errorCode = err.code || "Error_Code";
+  res.status(status).json({ message, error: errorCode });
 });
