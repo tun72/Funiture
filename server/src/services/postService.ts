6c5b1fa431da015ecfe3e40f6eaab437f65a2f2c
Tun Tun Myint <tunt72553@gmail.com>
2025-05-22 13:00:44 +0630

    sanitize-html-and-update-delete-post

--- server/src/services/postService.ts
+++ server/src/services/postService.ts
@@ -10,12 +10,12 @@ export type PostArgs = {
   body: string;
   category: string;
   image: string;
-  authorId: number;
+  authorId?: number;
   type: string;
   tags: string[];
 };
 export const createOnePost = async (postData: PostArgs) => {
-  let data: any = {
+  const data: any = {
     title: postData.title,
     content: postData.content,
     body: postData.body,
@@ -53,5 +53,66 @@ export const createOnePost = async (postData: PostArgs) => {
   }
   return prisma.post.create({
     data,
+  });
+};
+
+// update
+
+export const updateOnePost = async (postId: number, postData: PostArgs) => {
+  const data: any = {
+    title: postData.title,
+    content: postData.content,
+    body: postData.body,
+    image: postData.image,
+    category: {
+      connectOrCreate: {
+        where: { name: postData.category },
+        create: {
+          name: postData.category,
+        },
+      },
+    },
+    type: {
+      connectOrCreate: {
+        where: { name: postData.type },
+        create: {
+          name: postData.type,
+        },
+      },
+    },
+  };
+
+  if (postData.image) {
+    data.image = postData.image;
+  }
+
+  if (postData.tags && postData.tags.length !== 0) {
+    data.tags = {
+      set: [],
+      connectOrCreate: postData.tags.map((tagName: string) => ({
+        where: { name: tagName },
+        create: {
+          name: tagName,
+        },
+      })),
+    };
+  }
+  return prisma.post.update({
+    where: { id: postId },
+    data,
+  });
+};
+
+// delete
+export const deleteOnePost = async (id: number) => {
+  return prisma.post.delete({
+    where: { id },
+  });
+};
+
+// get
+export const getPostById = async (id: number) => {
+  return prisma.post.findUnique({
+    where: { id },
   });
 };
